0cc5bdc5db3bc278e08d1f5ba5a06c3ba33a8b58
Maria-Varghese <maria.varghese@mths.ca>
2024-10-08 20:22:25 -0700

    using Robotbit Neopixels

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria
+ * Created on: Sep 2024
+ * This program uses Robotbits Neopixels
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// off all colors
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Square)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+})
+
